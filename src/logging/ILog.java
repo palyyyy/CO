e27114f4f104ade7116da73534e8716567ffa970
Paly <112581464+palyyyy@users.noreply.github.com>
2023-03-06 13:16:34 +0300

    Update ILog.java

--- src/logging/ILog.java
+++ src/logging/ILog.java
@@ -1,4 +1,11 @@
 package logging;
 
 public interface ILog {
+  public void write(long value);
+
+    public void write(String value);
+
+    public void write(Object... values);
+
+    public void close();
 }
